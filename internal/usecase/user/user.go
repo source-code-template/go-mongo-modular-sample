d9010bacd14356fe2459461871032844b4f1e9ec
Duc Nguyen <minhduc140583@gmail.com>
2022-03-13 11:18:06 +0700

    Refactor model

--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -3,7 +3,7 @@ package user
 import "time"
 
 type User struct {
-	Id          string     `json:"id" gorm:"column:id;primary_key" bson:"_id" dynamodbav:"id" firestore:"id" avro:"id" match:"equal" validate:"required,max=40"`
+	Id          string     `json:"id" gorm:"column:id;primary_key" bson:"_id" dynamodbav:"id" firestore:"-" avro:"id" match:"equal" validate:"required,max=40"`
 	Username    string     `json:"username" gorm:"column:username" bson:"username" dynamodbav:"username" firestore:"username" avro:"username" match:"prefix" validate:"required,username,max=100"`
 	Email       string     `json:"email" gorm:"column:email" bson:"email" dynamodbav:"email" firestore:"email" avro:"email" match:"prefix" validate:"email,max=100"`
 	Phone       string     `json:"phone" gorm:"column:phone" bson:"phone" dynamodbav:"phone" firestore:"phone" avro:"phone" validate:"required,phone,max=18"`
